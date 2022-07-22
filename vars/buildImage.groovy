b9e22de1b93cb0d6d5e58e0fc471f4a5bbadd06f
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-07-22 10:44:36 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 20.225.57.67:8083/my-job:amineounissi-3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 20.225.57.67:8083"
-        sh 'docker push 20.225.57.67:8083/my-job:amineounissi-3.0'
+        sh 'docker build -t 104.214.55.140:8083/my-job:$IMAGE_NAME .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 104.214.55.140:8083"
+        sh 'docker push 104.214.55.140:8083/my-job:$IMAGE_NAME'
     }
 }
