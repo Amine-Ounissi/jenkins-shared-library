7e9f9f5a80f7c98626a6df1f79efaea293399a00
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-07-26 16:31:44 +0100

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the application..."
     sh'pwd'
-    sh'ls'
-    sh'JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn clean javadoc:jar package'
+    sh'ls'    
+    sh'mvn clean package'
     
 }
