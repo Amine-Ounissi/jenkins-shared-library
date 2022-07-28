caf15a58e021cfbb073740a23672489591d7ef73
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-07-28 16:42:12 +0100

    Update buildJar.groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -4,14 +4,5 @@ def call() {
     echo "building the application..."
     sh 'pwd'
     sh 'ls'
-    sh 'java -version'
-    sh 'mvn -version'
-    sh 'whereis java'
-    sh 'whereis maven'
-    sh 'echo $JAVA_HOME'
-    sh 'echo $MAVEN_HOME'
-    sh 'mvn clean install'
-
-
-    
+    sh 'mvn clean install'   
 }
