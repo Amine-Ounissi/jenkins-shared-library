38136f0f17ee8d30a7ab00071bf3b7eb923634df
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-09-07 16:44:25 +0100

    Create deployApp.groovy

---
+++ vars/deployApp.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo 'deploying  the docker image..."
+    withKubeConfig([credentialsId: 'kubeconfig-raw', serverUrl: '']) {
+        sh ('kubectl apply -f deployment.yaml')
+        sh('kubectl get deployment')
+        sh('kubectl get pods')
+        sh('kubectl get svc')
+    }
+}
