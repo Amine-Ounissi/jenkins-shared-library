b3ab160349e709251bc09bada7aa311dc44c10c0
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-09-09 01:42:59 +0100

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -2,7 +2,7 @@
 
 def call() {
     echo 'deploying  the docker image..."
-    withKubeConfig([credentialsId: 'kubeconfig-raw', serverUrl: '']) {
+    //withKubeConfig([credentialsId: 'kubeconfig-raw', serverUrl: '']) {
         sh 'kubectl apply -f deployment.yaml'
         sh 'kubectl get deployment'
         sh 'kubectl get pods'
